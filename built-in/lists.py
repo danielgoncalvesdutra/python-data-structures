a3d6a41166d49ddfaa14d6a2d6aebe42c06c260c
Daniel Gonçalves Dutra <daniel.dutra.45@gmail.com>
2022-09-26 17:10:35 -0300

    .

---
+++ built-in/lists.py
@@ -0,0 +1,15 @@
+# declarando uma lista númerica
+minha_lista = [1, 2, 3, 4, 5]
+
+# imprimindo toda a lista
+print(minha_lista)
+
+# qual o tipo da variável?
+print(type(minha_lista)) 
+
+#imprimindo itenm por item (usando o índice)
+print(minha_lista[0]) #1
+print(minha_lista[1]) #2
+print(minha_lista[2]) #3
+print(minha_lista[3]) #4
+print(minha_lista[4]) #5
---
+++ built-in/tuples.py
@@ -0,0 +1,7 @@
+# forma de declarar um tupla
+tupla = (1, 2, 3)
+
+# outra forma de declarar uma tupla
+tupla = 1, 2, 3
+
+print(type(tupla))
